8f934f173d50c9750d0b97569cd47e76540c066d
Patrick Sexton <PatrickJBSSexton@gmail.com>
2011-03-02 14:04:15 -0600

    adding files

--- PseudoRandomTextGenerator/TextGenerator.cs
+++ PseudoRandomTextGenerator/TextGenerator.cs
@@ -1,102 +1,125 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace PseudoRandomTextGenerator
 {
 	public class TextGenerator
 	{
-        private Dictionary<string, Dictionary<string, int>> _chain;
+		private readonly int _keySize;
+		private Dictionary<string, List<string>> _chain;
         private Random _random;
 
-		public TextGenerator()
+		public TextGenerator(int keySize = 2)
         {
-            _chain = new Dictionary<string, Dictionary<string, int>>();
+			if(keySize<1||keySize>5) throw new ArgumentOutOfRangeException("keySize","Can be 1-5");
+
+			_keySize = keySize;
+			_chain = new Dictionary<string, List<string>>();
             _random = new Random();
         }
 
         public void ReadTextFile(string filePath)
         {
-            TextReader reader = new StreamReader(filePath);
-            var text = reader.ReadToEnd();
-            reader.Close();
-
-            var tokens = Split(text);
-
-            AddTokensToChain(tokens);
+			ReadStreamAndInitialize(new StreamReader(filePath).BaseStream);
         }
+
+		public void ReadStream(Stream stream)
+		{
+			ReadStreamAndInitialize(stream);
+		}
+
+		public void ReadText(string thisIsMySeedData)
+		{
+			InitializeChain(thisIsMySeedData);
+		}
+
+		public string Write(int minLength, int maxLength)
+		{
+			// once we get a string longer than this we are done
+			// NOTE: minLength is a hard barrier, while maxLength is soft.
+			var length = _random.Next(minLength, maxLength);
+
+			// get the first key
+			var seedString = GetSeedString(_random.Next(_chain.Count));
+			var currentKey = seedString;
+			// keep track of the sentance length
+			var sentanceLength = seedString.Length;
+			// keep the words in a list, so we aren't doing more string manipulation than necessary
+			var words = new List<string> {seedString};
+			
+			string nextWord;
+
+			// while we haven't reached the length
+			// get the next word
+			// add it to the list
+			// increment the sentance length, add one for the space we will add in the join
+			// update the current key by getting the tail and appending the nextWord
+			while (sentanceLength < length)
+			{
+				nextWord = GetNextWord(currentKey);
+				words.Add(nextWord);
+				sentanceLength += (nextWord.Length + 1);
+				currentKey = string.Join(" ", currentKey.Split(Convert.ToChar(" ")).Skip(1)) + " " + nextWord;
+			}
+
+			return string.Join(" ",words);
+		}
+
+		private void ReadStreamAndInitialize(Stream stream)
+		{
+			TextReader reader = new StreamReader(stream);
+			var text = reader.ReadToEnd();
+			reader.Close();
+
+			InitializeChain(text);
+		}
+
+		private void InitializeChain(string text)
+		{
+			var tokens = Split(text);
+
+			if(tokens.Count < _keySize) throw new ArgumentException("The text is smaller than the key size");
+
+			AddTokensToChain(tokens);
+		}
+
+		static IList<string> Split(string subject)
+		{
+			var regex = new Regex(@"\s+");
+			return regex.Split(subject).ToList();
+		}
 
         private void AddTokensToChain(IList<string> tokens)
         {
-            for (var i = 2; i < tokens.Count; i++)
+            for (var i = _keySize; i < tokens.Count; i++)
             {
-                AddToken(tokens[i - 2], tokens[i - 1], tokens[i]);
+            	var nonModifiedClosurei = i;
+            	var keyTokens = Enumerable.Range(0, _keySize).Select(k => tokens[nonModifiedClosurei - (_keySize - k)].Trim());
+
+            	AddToken(keyTokens, tokens[nonModifiedClosurei].Trim());
             }
         }
 
-        private void AddToken(string key1, string key2, string value)
+        private void AddToken(IEnumerable<string> keyTokens, string value)
         {
-            var chainKey = string.Format("{0} {1}", key1, key2);
+        	var chainKey = string.Join(" ", keyTokens);
 
             if (_chain.ContainsKey(chainKey))
             {
-                if (!_chain[chainKey].ContainsKey(value)) _chain[chainKey].Add(value, 0);
+                if (!_chain[chainKey].Contains(value)) _chain[chainKey].Add(value);
             }
-            else _chain.Add(chainKey, new Dictionary<string, int>() { { value, 0 } });
+            else _chain.Add(chainKey, new List<string>() { value  });
         }
 
-        static IList<string> Split(string subject)
+        private string GetNextWord(string key)
         {
-            var tokens = new List<string>();
-            var regex = new Regex(@"\s+");
-            tokens.AddRange(regex.Split(subject));
-
-            return tokens;
-        }
-
-        public string Write(int minLength, int maxLength)
-        {
-            var length = _random.Next(minLength, maxLength);
-
-            var memoir = GetSeedString(_random.Next(_chain.Count)); ;
-            var parts = Split(memoir);
-            var key1 = parts[0];
-            var key2 = parts[1];
-            var nextWord = string.Empty;
-
-            while (memoir.Length < length)
-            {
-                nextWord = GetNextWord(key1, key2);
-
-                memoir += " " + nextWord;
-
-                key1 = key2;
-                key2 = nextWord;
-            }
-
-            return memoir;
-        }
-
-        private string GetNextWord(string key1, string key2)
-        {
-            var key = string.Format("{0} {1}", key1, key2);
 			if (_chain.ContainsKey(key))
 			{
-				var possibleWords = _chain[key];
-
-				var index = _random.Next(possibleWords.Count - 1);
-
-				var nextWord = string.Empty;
-				var count = 0;
-				foreach (var possibleWord in possibleWords)
-				{
-					nextWord = possibleWord.Key;
-					count++;
-					if (count == index) break;
-				}
-
-				return nextWord;
+				var index = _random.Next(_chain[key].Count - 1);
+				return _chain[key][index];
 			}
 
         	return GetSeedString(_random.Next(_chain.Count));
@@ -104,9 +127,7 @@ namespace PseudoRandomTextGenerator
 
         private string GetSeedString(int keyIndex)
         {
-            var chainKeys = new string[_chain.Count];
-            _chain.Keys.CopyTo(chainKeys, 0);
-            return chainKeys[keyIndex];
+        	return _chain.Keys.ToArray()[keyIndex];
         }
 	}
 }
